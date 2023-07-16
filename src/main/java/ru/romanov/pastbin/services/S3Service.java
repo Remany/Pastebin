9371e859c19ec4d13ae26ebc0cdcc8b9fb9b73fe
Romanov Leonid <romanovleonid82@gmail.com>
2023-07-17 08:45:21 +1000

    add aws config and setup

---
+++ src/main/java/ru/romanov/pastbin/services/S3Service.java
@@ -0,0 +1,29 @@
+package ru.romanov.pastbin.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+
+@Service
+public class S3Service {
+    private final S3Client s3Client;
+
+    @Autowired
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public void createBucket(String bucketName) {
+        CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+                .bucket(bucketName)
+                .build();
+
+        s3Client.createBucket(createBucketRequest);
+    }
+
+    public ListBucketsResponse listBuckets() {
+        return s3Client.listBuckets();
+    }
+}
