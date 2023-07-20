68f23107f1ce705a615e151934b669148178873c
Romanov Leonid <romanovleonid82@gmail.com>
2023-07-21 00:50:12 +1000

    delete jwt logic. create session/cookies logic of work application

--- src/main/java/ru/romanov/pastbin/config/SecurityConfig.java
+++ src/main/java/ru/romanov/pastbin/config/SecurityConfig.java
@@ -10,21 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.romanov.pastbin.services.PersonDetailsService;
 
 import static org.springframework.http.HttpMethod.OPTIONS;
 
 @Configuration
 public class SecurityConfig {
-    private final PersonDetailsService personDetailsService;
-    private final JWTFilter jwtFilter;
-
-    @Autowired
-    public SecurityConfig(PersonDetailsService personDetailsService, JWTFilter jwtFilter) {
-        this.personDetailsService = personDetailsService;
-        this.jwtFilter = jwtFilter;
-    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -42,11 +33,12 @@ public class SecurityConfig {
                                 .hasAnyRole("USER", "ADMIN"))
                 .sessionManagement((session) ->
                         session
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                                .invalidSessionUrl("/pastbin/auth/login?expired")
+                                .maximumSessions(1)
+                                .maxSessionsPreventsLogin(false))
 
                 .httpBasic(Customizer.withDefaults());
-
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
