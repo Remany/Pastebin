853f070bd359ca14527bf9bce26beca9221dbf3d
Romanov Leonid <romanovleonid82@gmail.com>
2023-07-21 00:57:22 +1000

    create registration logic

---
+++ src/main/java/ru/romanov/pastbin/services/RegistrationService.java
@@ -0,0 +1,27 @@
+package ru.romanov.pastbin.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.romanov.pastbin.models.Person;
+import ru.romanov.pastbin.repositories.PersonRepository;
+
+@Service
+public class RegistrationService {
+    private final PasswordEncoder passwordEncoder;
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public RegistrationService(PasswordEncoder passwordEncoder, PersonRepository personRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.personRepository = personRepository;
+    }
+
+    @Transactional
+    public void registration(Person person) {
+        passwordEncoder.encode(person.getPassword());
+        person.setRole("ROLE_USER");
+        personRepository.save(person);
+    }
+}
