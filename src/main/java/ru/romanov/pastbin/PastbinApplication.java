75833c186763632bc0afae029e223f4d4f88e023
Romanov Leonid <romanovleonid82@gmail.com>
2023-07-20 21:06:06 +1000

    delete init method from PastbinApplication

--- src/main/java/ru/romanov/pastbin/PastbinApplication.java
+++ src/main/java/ru/romanov/pastbin/PastbinApplication.java
@@ -1,32 +1,11 @@
 package ru.romanov.pastbin;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.romanov.pastbin.services.S3Service;
-import software.amazon.awssdk.services.s3.model.Bucket;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
-
-import java.util.List;
 
 @SpringBootApplication
 public class PastbinApplication {
-
-	@Autowired
-	private S3Service s3Service;
-
 	public static void main(String[] args) {
 		SpringApplication.run(PastbinApplication.class, args);
-	}
-	@PostConstruct
-	public void init() {
-		s3Service.createBucket("some");
-
-		ListBucketsResponse bucketsResponse = s3Service.listBuckets();
-		List<Bucket> buckets = bucketsResponse.buckets();
-		for (Bucket bucket : buckets) {
-			System.out.println("Bucket: " + bucket.name());
-		}
 	}
 }
