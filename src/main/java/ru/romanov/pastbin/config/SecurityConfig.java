a5a1b87bd52d8ee28a44d5e119a18289719fa8ba
Romanov Leonid <romanovleonid82@gmail.com>
2023-07-21 00:33:54 +1000

    back to JWT logic

--- src/main/java/ru/romanov/pastbin/config/SecurityConfig.java
+++ src/main/java/ru/romanov/pastbin/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.romanov.pastbin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,11 +10,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.romanov.pastbin.services.PersonDetailsService;
 
 import static org.springframework.http.HttpMethod.OPTIONS;
 
 @Configuration
 public class SecurityConfig {
+    private final PersonDetailsService personDetailsService;
+    private final JWTFilter jwtFilter;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService, JWTFilter jwtFilter) {
+        this.personDetailsService = personDetailsService;
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -23,20 +34,19 @@ public class SecurityConfig {
                         requests
                                 .requestMatchers(OPTIONS)
                                 .permitAll()
-                                .requestMatchers("/pastebin/auth/login", "/pastebin/auth/registration", "/error")
+                                .requestMatchers("/pastbin/auth/login", "/pastbin/auth/registration", "/error")
                                 .permitAll()
-                                .requestMatchers("/pastebin/test") // TODO убрать
+                                .requestMatchers("/pastbin/test") // TODO убрать
                                 .permitAll()
                                 .anyRequest()
                                 .hasAnyRole("USER", "ADMIN"))
                 .sessionManagement((session) ->
                         session
-                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                                .invalidSessionUrl("/pastebin/auth/login?expired")
-                                .maximumSessions(1)
-                                .maxSessionsPreventsLogin(false))
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .httpBasic(Customizer.withDefaults());
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
