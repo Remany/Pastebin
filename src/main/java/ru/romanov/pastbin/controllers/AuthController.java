1831c34712a0452faaf5e74d33ccc4cac59f25ba
Romanov Leonid <romanovleonid82@gmail.com>
2023-07-22 22:44:45 +1000

    minor changes

--- src/main/java/ru/romanov/pastbin/controllers/AuthController.java
+++ src/main/java/ru/romanov/pastbin/controllers/AuthController.java
@@ -4,8 +4,6 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
