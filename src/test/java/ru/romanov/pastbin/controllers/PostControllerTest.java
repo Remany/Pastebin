6796c61f138463145ecfac42c4f2e7a13926a180
Romanov Leonid <romanovleonid82@gmail.com>
2023-08-04 19:40:22 +1000

    complete test method of creating post

--- src/test/java/ru/romanov/pastbin/controllers/PostControllerTest.java
+++ src/test/java/ru/romanov/pastbin/controllers/PostControllerTest.java
@@ -1,56 +1,51 @@
 package ru.romanov.pastbin.controllers;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import ru.romanov.pastbin.dto.PostDTO;
-import ru.romanov.pastbin.security.JWTUtil;
+import ru.romanov.pastbin.models.Post;
 import ru.romanov.pastbin.services.PostService;
 import ru.romanov.pastbin.services.S3Service;
 
 import java.security.Principal;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-@WebMvcTest(PostController.class)
-class PostControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private PostService postService;
-
-    @MockBean
+@ExtendWith(MockitoExtension.class)
+public class PostControllerTest {
+    @Mock
     private S3Service s3Service;
-    @MockBean
-    private JWTUtil jwtUtil;
-
-
+    @Mock
+    private PostService postService;
+    @Mock
+    private ModelMapper modelMapper;
+    @InjectMocks
+    private PostController postController;
     @Test
-    public void testCreatePost() throws Exception {
+    public void  shouldCreatedPost() {
         PostDTO postDTO = new PostDTO();
-        postDTO.setText("some text"); postDTO.setTitle("some title");
+        postDTO.setText("Test post content");
 
-        Principal principal = () -> "testuser";
+        Principal principal = mock(Principal.class);
+        String expectedObjectKey = "some_object_key";
+        String expectedUrl = "http://localhost:8080/pastebin/posts/get/some_generated_url";
 
-        when(postService.createUrl()).thenReturn("someurl");
-        when(s3Service.uploadText(any(), any())).thenReturn("someObjectKey");
+        Post expectedPost = new Post();
+        when(postService.createUrl()).thenReturn("some_generated_url");
+        when(modelMapper.map(postDTO, Post.class)).thenReturn(expectedPost);
+        when(s3Service.uploadText(expectedPost.getText(), principal)).thenReturn(expectedObjectKey);
 
-        mockMvc.perform(post("/pastebin/posts/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(postDTO))
-                        .principal(principal))
-                        .andExpect(status().isOk())
-                        .andExpect(content().string("http://localhost:8080/pastebin/posts/get/1234"));
-    }
-    @Test
-    void getPost() {
+        ResponseEntity<String> responseEntity = postController.createPost(postDTO, principal);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedUrl, responseEntity.getBody());
+
+        verify(postService, times(1)).save(expectedPost, principal);
     }
 }
