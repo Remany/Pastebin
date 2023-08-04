3dbcb6218a1d3bedd49bfe2c2438614e6e6fe57b
Romanov Leonid <romanovleonid82@gmail.com>
2023-08-04 20:10:51 +1000

    create test method for PersonService class

---
+++ src/test/java/ru/romanov/pastbin/services/PersonServiceTest.java
@@ -0,0 +1,47 @@
+package ru.romanov.pastbin.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.romanov.pastbin.models.Person;
+import ru.romanov.pastbin.repositories.PersonRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+    @Mock
+    private PersonRepository personRepository;
+    @InjectMocks
+    private PersonService personService;
+    @Test
+    void shouldGettingPersonByUsernameIfUserFound() {
+        Person expectedPerson = new Person();
+        expectedPerson.setUsername("some name");
+        expectedPerson.setPassword("some password");
+
+        when(personRepository.findPersonByUsername(expectedPerson.getUsername()))
+                .thenReturn(Optional.of(expectedPerson));
+
+        Optional<Person> resultPerson = personRepository.findPersonByUsername(expectedPerson.getUsername());
+
+        assertTrue(resultPerson.isPresent());
+        assertEquals(expectedPerson.getUsername(), resultPerson.get().getUsername());
+    }
+
+    @Test
+    void shouldGettingPersonByUsernameIfUserNotFound() {
+        String username = "some name";
+
+        when(personRepository.findPersonByUsername(username)).thenReturn(Optional.empty());
+
+        Optional<Person> resultPerson = personRepository.findPersonByUsername(username);
+
+        assertFalse(resultPerson.isPresent());
+    }
+}
